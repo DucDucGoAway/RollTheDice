632c6c7bb917fcc693d39a1c992abc27d6b7d2bd
Taifun <taifun@Taifuns-MacBook-Pro.local>
2023-01-19 15:33:40 +0100

    Initial commit

---
+++ app/src/main/java/com/duc/rollthedice/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.duc.rollthedice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.duc.rollthedice.ui.theme.RollTheDiceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RollTheDiceTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    DiceRollWithButtonImage()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DiceRollWithButtonImage(
+    modifier: Modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+) {
+
+    var result by remember { mutableStateOf(1)}
+    val imageResource = when(result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = imageResource),
+            contentDescription = result.toString(),
+            modifier = Modifier
+                .padding(bottom = 24.dp)
+        )
+        Button(
+            onClick = { result = (1..6).random() },
+            modifier = Modifier
+                .height(50.dp)
+                .width(100.dp)
+        ) {
+            Text(stringResource(R.string.roll))
+        }
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun DicePreview() {
+    RollTheDiceTheme {
+        DiceRollWithButtonImage()
+    }
+}
